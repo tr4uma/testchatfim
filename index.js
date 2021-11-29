c11d784837e7a2bf3f033d2658acc7e08b39f103
Marco Dagrada <marco.dagrada@telnext.com>
2021-11-29 16:12:55 +0100

    test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 3010
 const express = require('express')
 const app = express()
-const port = 8080
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
