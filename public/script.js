57c4002bccf624612422625c0742968867c8cff3
Mauri, Stefano <stmauri@deloitte.it>
2021-12-01 11:38:48 +0100

    Logged in view

--- public/script.js
+++ public/script.js
@@ -53,14 +53,6 @@ function initESW(gslbBaseURL) {
   if (userData !== null && typeof userData !== "undefined") {
     // User Logged In
     console.log("User Logged In");
-    // Override setup
-    // embedded_svc.settings.extraPrechatFormDetails = [
-    //   {
-    //     label: "issue",
-    //     value: "Overriding your setup",
-    //     displayToAgent: true,
-    //   },
-    // ];
     // Prepopulate fields
     embedded_svc.settings.prepopulatedPrechatFields = {
       FirstName: userData.firstName,
@@ -108,19 +100,6 @@ function initESW(gslbBaseURL) {
     //     ],
     //   },
     // ];
-    //embedded_svc.settings.defaultMinimizedText = '...'; //(Defaults to Chat with an Expert)
-    //embedded_svc.settings.disabledMinimizedText = '...'; //(Defaults to Agent Offline)
-
-    //embedded_svc.settings.loadingText = ''; //(Defaults to Loading)
-    //embedded_svc.settings.storageDomain = 'yourdomain.com'; //(Sets the domain for your deployment so that visitors can navigate subdomains during a chat session)
-
-    // Settings for Chat
-    //embedded_svc.settings.directToButtonRouting = function(prechatFormData) {
-    // Dynamically changes the button ID based on what the visitor enters in the pre-chat form.
-    // Returns a valid button ID.
-    //};
-    //embedded_svc.settings.fallbackRouting = []; //An array of button IDs, user IDs, or userId_buttonId
-    //embedded_svc.settings.offlineSupportMinimizedText = '...'; //(Defaults to Contact Us)
     embedded_svc.init(
       'https://fulmineinmano--devshared.my.salesforce.com',
       'https://devshared-sf-liveagent.cs101.force.com/liveAgentSetupFlow',
@@ -136,13 +115,25 @@ function initESW(gslbBaseURL) {
         isOfflineSupportEnabled: false
       }
     );
-
-    var x = document.getElementsByClassName("fieldList");
-    console.log(x)
-    for (var i = 0; i < 3; i++) {
-      x.childNodes[i].style.visibility = "hidden";
-      console.log(x.childNodes[i]);
-    }
+  } else {
+    // Guest user
+    console.log("Guest User");
+    embedded_svc.init(
+      "https://fulmineinmano--devshared.my.salesforce.com",
+      "https://devshared-sf-liveagent.cs101.force.com/liveAgentSetupFlow",
+      gslbBaseURL,
+      "00D1X0000000Npj",
+      "Website_Prospect_FIM",
+      {
+        baseLiveAgentContentURL:
+          "https://c.la1-c1cs-fra.salesforceliveagent.com/content",
+        deploymentId: "5721X0000004ECE",
+        buttonId: "5731X0000004DHg",
+        baseLiveAgentURL: "https://d.la1-c1cs-fra.salesforceliveagent.com/chat",
+        eswLiveAgentDevName: "Website_Prospect_FIM",
+        isOfflineSupportEnabled: true,
+      }
+    );
   }
 
 }
