0c5fc8b637ea4b19a0f82aafcdc2d71cd8966331
Mauri, Stefano <stmauri@deloitte.it>
2022-01-19 17:04:43 +0100

    Fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 3010
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'));
+app.use(express.static('public/'));
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
